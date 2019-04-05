98ee4fdb24c566b6ce8736fbdfea9eb153e87342
Ngoc Duy Mai <maingo01@students.zhaw.ch>
2019-04-05 23:52:30 +0200

    implementation of jwt authentication

--- source/backend/index.js
+++ source/backend/index.js
@@ -1,10 +1,51 @@
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+// CORS
+app.all('/*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+// AUTHENTICATION
+app.post('/api/auth/login', function(req, res) {
+  const loginDetails = {
+    username: req.body.username,
+    password: req.body.password
+  };
+
+  if (!isValidCredentials(loginDetails)) {
+    res.status(401).json({ message: 'Authentication failed. User not found.' });
+  }
+  const jwtToken = jwt.sign(
+    { username: loginDetails.username },
+    'supersecretkey'
+  );
+
+  res.json({
+    jwtToken: jwtToken
+  });
+});
+
+function isValidCredentials({ username, password }) {
+  // TODO hardcoded user credentials
+  return username === 'test' && password === 'test';
+}
+
+// START APP
 const server = app.listen(8080, () => {
   const host = server.address().address;
   const port = server.address().port;
