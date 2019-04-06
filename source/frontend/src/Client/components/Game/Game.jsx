2ac0fc755f92b32ccc0591203aeccd76ef52c843
Ngoc Duy Mai <maingo01@students.zhaw.ch>
2019-04-06 13:27:25 +0200

    added reward modal

--- source/frontend/src/Client/components/Game/Game.jsx
+++ source/frontend/src/Client/components/Game/Game.jsx
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
 
 import './Game.sass';
+import RewardModal from '../RewardModal/RewardModal';
 
 class Game extends Component {
-  state = {};
+  state = { rewardModalVisible: true };
 
   handleChange = (event, value) => {
     this.setState({ value });
   };
 
+  showRewardModal = () => {
+    this.setState({ rewardModalVisible: true });
+  };
+
+  playFancyAnimation = () => {
+    // do some fancy animation for collecting reward, then dismiss modal
+    this.setState({ rewardModalVisible: false });
+  };
+
+  dismissRewardModal = () => {
+    this.setState({ rewardModalVisible: false });
+  };
+
   render() {
     return (
       <div className='Game'>
+        <RewardModal
+          visible={this.state.rewardModalVisible}
+          playAnimation={this.playFancyAnimation}
+          onClose={this.dismissRewardModal}
+        />
         <h1>Game</h1>
       </div>
     );
