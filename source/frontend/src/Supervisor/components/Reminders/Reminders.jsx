6383c04d8a0b98ae77959e3949c060fb46e53fae
bachmdo <bachdomi1@gmail.com>
2019-04-06 16:11:25 +0200

    Added listener for challenge/reminder in Chat.js

--- source/frontend/src/Supervisor/components/Reminders/Reminders.jsx
+++ source/frontend/src/Supervisor/components/Reminders/Reminders.jsx
@@ -10,22 +10,63 @@ import Button from '@material-ui/core/Button';
 
 import './Reminders.sass';
 
-class Reminders extends Component {
-  state = {};
+import io from 'socket.io-client';
+import globalStore from '../../../Shared/stores/GlobalStore';
 
-  handleChange = (event, value) => {
-    this.setState({ value });
+class Reminders extends Component {
+  state = {
+    eventname: '',
+    detail: '',
+    date: '',
+    time: '',
+    socket: io('http://localhost:8080')
+  };
+
+  componentDidMount() {
+    this.state.socket.on('connect', socket => {
+      console.log(globalStore.userType);
+      this.state.socket.emit('join', { type: globalStore.userType });
+      this.state.socket.on('getMsg', data => {
+        console.log('message:', data);
+        this.setState(prevState => ({
+          messages: [...prevState.messages, data]
+        }));
+      });
+      this.state.socket.on('getChallenge', data => {
+        console.log('challenge:', data);
+        setTimeout(() => {
+          //TODO: show duys modal with setState
+        }, 10000);
+      });
+    });
+  }
+
+  handleInput = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleCreate = () => {
+    const challenge = {
+      eventname: this.state.eventname,
+      detail: this.state.detail,
+      date: this.state.date,
+      time: this.state.time
+    };
+    this.state.socket.emit('challenge', {
+      type: globalStore.userType,
+      challenge: challenge
+    });
   };
 
   render() {
     const { eventname, info } = this.state;
     return (
-      <div className='Reminders'>
+      <div className="Reminders">
         <h1>Reminders</h1>
-        <div className='Reminders-Questions'>
+        <div className="Reminders-Questions">
           <ExpansionPanel>
             <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-              <Typography className='heading'>
+              <Typography className="heading">
                 Mike Interview 8.04.19 12:00
               </Typography>
             </ExpansionPanelSummary>
@@ -38,7 +79,7 @@ class Reminders extends Component {
           </ExpansionPanel>
           <ExpansionPanel>
             <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-              <Typography className='heading'>
+              <Typography className="heading">
                 John Interview 17.07.19 7:00
               </Typography>
             </ExpansionPanelSummary>
@@ -50,7 +91,7 @@ class Reminders extends Component {
           </ExpansionPanel>
           <ExpansionPanel>
             <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-              <Typography className='heading'>
+              <Typography className="heading">
                 Daniel Daily Morning Wake up / Checkin 7:00
               </Typography>
             </ExpansionPanelSummary>
@@ -59,50 +100,50 @@ class Reminders extends Component {
             </ExpansionPanelDetails>
           </ExpansionPanel>
           <h1>Create new Event</h1>
-          <div className='ReminderForm'>
+          <div className="ReminderForm">
             <TextField
-              label='Event Name'
-              className='TextField'
-              type='user'
-              autoComplete='user'
-              margin='normal'
-              variant='outlined'
-              name='eventname'
+              label="Event Name"
+              className="TextField"
+              type="user"
+              autoComplete="user"
+              margin="normal"
+              variant="outlined"
+              name="eventname"
               value={eventname}
               onChange={this.handleInput}
             />
             <TextField
-              label='More Info'
-              className='TextField'
-              type='text'
-              name='info'
-              margin='normal'
-              variant='outlined'
+              label="More Info"
+              className="TextField"
+              type="text"
+              name="detail"
+              margin="normal"
+              variant="outlined"
               value={info}
               onChange={this.handleInput}
             />
             <TextField
-              label='Date'
-              className='TextField'
-              type='text'
-              name='info'
-              margin='normal'
-              variant='outlined'
+              label="Date"
+              className="TextField"
+              type="text"
+              name="date"
+              margin="normal"
+              variant="outlined"
               value={info}
               onChange={this.handleInput}
             />
             <TextField
-              label='Time'
-              className='TextField'
-              type='text'
-              name='info'
-              margin='normal'
-              variant='outlined'
+              label="Time"
+              className="TextField"
+              type="text"
+              name="time"
+              margin="normal"
+              variant="outlined"
               value={info}
               onChange={this.handleInput}
             />
           </div>
-          <Button variant='outlined' x onClick={this.handleLogin}>
+          <Button variant="outlined" x onClick={this.handleCreate}>
             Create Event
           </Button>
         </div>
